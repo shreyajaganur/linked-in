72acfd40d98d19738a74af5f04bbd558b1e8cb13
shreyajaganur <shreyajaganur@gmail.com>
2025-01-15 21:44:39 +0530

    m2

--- src/main/java/proj1/App.java
+++ src/main/java/proj1/App.java
@@ -5,6 +5,6 @@ package proj1;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 }
