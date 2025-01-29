95f2b8e3c936cb02965a52c49315789f57d2f806
Shreya Jaganur <99599225+shreyajaganur@users.noreply.github.com>
2025-01-29 15:16:24 +0530

    Update App.java
    new

--- src/main/java/proj1/App.java
+++ src/main/java/proj1/App.java
@@ -5,6 +5,6 @@ package proj1;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World and hello touka !");
+        System.out.println("Hello World and sumeru!");
     }
 }
